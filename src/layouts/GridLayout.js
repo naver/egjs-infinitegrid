7e7a59a64336e952eb87b8c97f440880ddc94755
younkue choi <younkue.choi@navercorp.com>
2017-09-29 16:32:42 +0900

    feat(Layout): add GridLayout

--- src/layouts/GridLayout.js
+++ src/layouts/GridLayout.js
@@ -1,23 +1,36 @@
-const APPEND = true;
-const PREPEND = false;
+import {APPEND, PREPEND, VERTICAL, HORIZONTAL, STYLE, DEFAULT_OPTIONS} from "./Constants";
+
+// ALIGN
+const START = "start";
+const CENTER = "center";
+const END = "end";
+const JUSTIFY = "justify";
 
 function fill(length, pos) {
 	return new Array(length).fill(pos);
 }
 class GridLayout {
 	constructor(options = {}) {
-		this.options = Object.assign(
+		this.options = Object.assign({},
+			DEFAULT_OPTIONS,
 			{
-				direction: "vertical",
-				margin: 0,
+				align: START,
 			},
 			options);
-		this.isHorizontal = this.options.direction === "horizontal";
-		this.columnSize = 0;
-		this.coulmnLength = 0;
+		this._viewport = {};
+		this._isHorizontal = this.options.direction === HORIZONTAL;
+		this._columnSize = 0;
+		this._columnLength = 0;
+		this._style = this.getStyleNames();
+	}
+	getStyleNames() {
+		const direction = this.options.direction in STYLE ? this.options.direction : VERTICAL;
+		const style = STYLE[direction];
+
+		return style;
 	}
 	getPoints(outlines) {
-		const pos = this.isHorizontal ? "left" : "top";
+		const pos = this._isHorizontal ? "left" : "top";
 
 		return outlines.map(outline => outline[pos]);
 	}
@@ -25,17 +38,30 @@ class GridLayout {
 		const margin = this.options.margin;
 		// if direction is horizontal, fixed dimension is height
 		// if direction is vertical, fixed dimension is width
-		const sizeName = this.isHorizontal ? "height" : "width";
+		const sizeName = this._isHorizontal ? "height" : "width";
+		const columnSize = item.size[sizeName];
 
-		this.columnSize = item.size[sizeName];
-		this.columnLength = parseInt((this[sizeName] + margin) / (this.columnSize + margin), 10);
+		this._columnSize = columnSize;
+		this._columnLength = parseInt((this._viewport[sizeName] + margin) / (columnSize + margin), 10);
 	}
-	_insert(items, outline, isAppend) {
-		const columnSize = this.columnSize;
+	_layout(items, outline, isAppend) {
 		const length = items.length;
 		const margin = this.options.margin;
-		const isHorizontal = this.isHorizontal;
-		const sizeName = isHorizontal ? "width" : "height";
+		const align = this.options.align;
+		const style = this._style;
+
+		const size1Name = style.size1;
+		const size2Name = style.size2;
+		const pos1Name = style.pos1;
+		const pos2Name = style.pos2;
+		const endPos1Name = style.endPos1;
+		const endPos2Name = style.endPos2;
+		const columnSize = this._columnSize;
+		const columnLength = this._columnLength;
+
+		const viewSize2 = this._viewport[size2Name];
+		const viewDist = (viewSize2 - (columnSize + margin) * columnLength + margin);
+
 		const pointCaculateName = isAppend ? "min" : "max";
 		const startOutline = outline.slice();
 		const endOutline = outline.slice();
@@ -43,98 +69,93 @@ class GridLayout {
 		for (let i = 0; i < length; ++i) {
 			const point = Math[pointCaculateName](...endOutline);
 			const index = endOutline.indexOf(point);
-			const item = items[i];
-			const size = item.size[sizeName];
-			const pos1 = isAppend ? point : point - margin - size;
-			const endPos1 = pos1 + size + margin;
-			const pos2 = (columnSize + margin) * index;
+			const item = items[isAppend ? i : length - 1 - i];
+			const size1 = item.size[size1Name];
+			const size2 = item.size[size2Name];
+			const pos1 = isAppend ? point : point - margin - size1;
+			const endPos1 = pos1 + size1 + margin;
+			let pos2 = (columnSize + margin) * index;
+
+			// ALIGN
+			if (align === CENTER) {
+				pos2 += viewDist / 2;
+			} else if (align === END) {
+				pos2 += viewDist + columnSize - size2;
+			} else if (align === JUSTIFY) {
+				pos2 = (viewSize2 - columnSize) / (columnLength - 1) * index;
+			}
 			const endPos2 = pos2 + columnSize + margin;
 
 			// tetris
-			items[i].rect = {
-				left: isHorizontal ? pos1 : pos2,
-				right: isHorizontal ? endPos1 : endPos2,
-				top: isHorizontal ? pos2 : pos1,
-				bottom: isHorizontal ? endPos2 : endPos1,
+			item.rect = {
+				[pos1Name]: pos1,
+				[pos2Name]: pos2,
+				[endPos1Name]: endPos1,
+				[endPos2Name]: endPos2,
 			};
-			items[i].column = index;
-
+			item.column = index;
 			endOutline[index] = isAppend ? endPos1 : pos1;
 		}
-		if (isAppend) {
-			return {
-				start: startOutline,
-				end: endOutline,
-			};
-		}
+		// if append items, startOutline is low, endOutline is high
+		// if prepend items, startOutline is high, endOutline is low
 		return {
-			start: endOutline,
-			end: startOutline,
+			start: isAppend ? startOutline : endOutline,
+			end: isAppend ? endOutline : startOutline,
+		};
+	}
+	_insert(items, outline, type) {
+		const clone = items.map(item => Object.assign({}, item));
+
+		let startOutline = outline;
+
+		if (!this._columnLength) {
+			this.checkColumn(items[0]);
+		}
+		if (outline.length !== this._columnLength) {
+			startOutline = fill(this._columnLength, Math[type === APPEND ? "min" : "max"](...outline));
+		}
+
+		const result = this._layout(clone, startOutline, type);
+
+		return {
+			items: clone,
+			outlines: result,
 		};
 	}
 	append(items, outline) {
-		const clone = items.map(item => ({
-			size: Object.assign({}, item.size),
-		}));
-		let startOutline = outline;
-
-		if (!this.columnLength) {
-			this.checkColumn(items[0]);
-		}
-		if (outline.length !== this.columnLength) {
-			startOutline = fill(this.columnLength, Math.min(...outline));
-		}
-		const result = this._insert(clone, startOutline, APPEND);
-
-		return {
-			items: clone,
-			outlines: result,
-		};
+		return this._insert(items, outline, APPEND);
 	}
 	prepend(items, outline) {
-		const clone = items.map(item => ({
-			size: Object.assign({}, item.size),
-		}));
-		let startOutline = outline;
-
-		if (!this.columnLength) {
-			this.checkColumn(items[0]);
-		}
-		if (outline.length !== this.columnLength) {
-			startOutline = fill(this.columnLength, Math.max(...outline));
-		}
-		const result = this._insert(clone, startOutline, PREPEND);
-
-		return {
-			items: clone,
-			outlines: result,
-		};
+		return this._insert(items, outline, PREPEND);
 	}
 	layout(groups, outline) {
 		this.checkColumn(groups[0].items[0]);
 		// if outlines' length and columns' length are now same, re-caculate outlines.
 		let startOutline;
 
-		if (outline.length !== this.columnLength) {
+		if (outline.length !== this._columnLength) {
 			const pos = Math.min(...outline);
 
 			// re-layout items.
-			startOutline = fill(this.columnLength, pos);
+			startOutline = fill(this._columnLength, pos);
 		} else {
 			startOutline = outline.slice();
 		}
 		groups.forEach(group => {
 			const items = group.items;
-			const result = this._insert(items, startOutline, APPEND);
+			const result = this._layout(items, startOutline, APPEND);
 
 			group.outlines = result;
 			startOutline = result.end;
 		});
 	}
 	setViewport(width, height) {
-		this.width = width;
-		this.height = height;
+		const viewport = this._viewport;
+
+		viewport.width = width;
+		viewport.height = height;
 	}
 }
 
 export default GridLayout;
+
