959b0dc89017116206a8ae7b16e8504f26a431af
Daybrush <itoolsg@users.noreply.github.com>
2017-09-29 10:36:44 +0900

    chore(Layout): fixed JustifiedLayout for group (#47)
    * feat(Layout): add GoogleLayout
    * feat(Layout): add GoogleLayout on outline
    * test(Layout): test GoogleLayout append/prepend
    * test(Layout): test GoogleLayout  - remove Item
    * chore(Layout): move folder lib
    * chore(Layout): move dijkstra file
    * chore(Layout): move Render file to lib
    * chore(Layout): fixed name GoogleLayout to JustifiedLayout
    * chore(Layout): fixed JustifiedLayout for group
    * chore(Layout): add test file

--- test/manual/layouts/js/demo.js
+++ test/manual/layouts/js/demo.js
@@ -1,25 +1,102 @@
 function imgSrc(v) {
 	return "../../../demo/assets/image/" + (((v + 1) % 60) + 1) + ".jpg";
 }
-function getItems(groupNo) {
-groupNo *= 30;
-var items = [];
-
-for (var i = 0; i < 30; i++) {
-  items.push('<div class="item"><div class="thumbnail"><img src="' + imgSrc(groupNo + i) + '" /><div class="caption"><p><a href="http://www.google.com/">Cras justo odio bla bla bla bla bla bla bla bla</a></p></div></div></div>');
+function getWidth() {
+	return  parseInt(Math.random() * 200 + 300, 10);
 }
-return items;
+function getHeight() {
+	return  parseInt(Math.random() * 200 + 300, 10);
+}
+function makeElement(item) {
+	const dummy = document.createElement("div");
+
+	dummy.innerHTML = item.html;
+	item.el = dummy.children[0];
+	item.el.style.width = item.size.width + "px";
+	item.el.style.height = item.size.height + "px";
+	document.querySelector("#grid").appendChild(item.el);
+}
+function getItem(i) {
+	const item = {
+		html: '<div class="item" data-item='+i+'><div class="thumbnail"><img src="' + imgSrc(i) + '" /><div class="caption"><p><a href="http://www.google.com/">Cras justo odio bla bla bla bla bla bla bla bla</a></p></div></div></div>',
+		size: {
+			width: getWidth(),
+			height: getHeight()
+		},
+		rect: {
+
+		}
+	};
+	makeElement(item);
+
+	return item;
+}
+function getItems() {
+	const items = [];
+	for (i = 0; i < 22; ++i) {
+		items.push(getItem(i));
+	}
+	return items;
 }
 
-var timer;
-var grid = document.getElementById("grid");
-var ig = new eg.InfiniteGrid("#grid", {
-		count: 4000,
-  defaultGroupKey: 0
+function getTopline(defaultOutline = [0]) {
+	if (!_items.length) {
+		return defaultOutline;
+	}
+	return _items[0].outlines.start;
+}
+function getBottomline(defaultOutline = [0]) {
+	if (!_items.length) {
+		return defaultOutline;
+	}
+	return _items[_items.length - 1].outlines.end;
+}
+
+let _items = [];
+
+function append(line) {
+	const group = parseInt(Math.random() * 10000);
+	const items = getItems();
+	const result = a.append(items, getBottomline(line));
+
+
+	result.items.forEach((item, i) => {
+		item.el = items[i].el;
+		item.groupKey = group;
 	});
+	result.groupKey = result.items[0].groupKey;
+	_items.push(result);
+	render(result.items);
+}
+function prepend(line) {
+	const group = parseInt(Math.random() * 10000);
+	const items = getItems();
+	const result = a.prepend(items, getTopline(line));
+	result.items.forEach((item, i) => {
+		item.el = items[i].el;
+		item.groupKey = group;
+	});
+	result.groupKey = result.items[0].groupKey;
+	_items.splice(0, 0, result);
+	render(result.items);
+}
+function _remove(index) {
+	const removed = _items[0].items.splice(index, 1);
+	removed[0].el.remove();
+}
+function remove(index) {
+	console.log(_items);
+	_remove(index);
+	a.layout(_items, getTopline());
+	_items.forEach(group => {
+		render(group.items);
+	})
+}
 
+function layout() {
+	a.layout(_items, [400]);
+	_items.forEach(group => {
+		render(group.items);
+	})
 
-ig.once("layoutComplete", function (e) {
-	grid.style.opacity = 1;
-});
-ig.append(getItems(0), 0);
+}
