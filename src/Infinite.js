ff28a0e95a691044a74a3b9730099afb49d589e0
sculove <sculove@gmail.com>
2017-10-18 19:02:28 +0900

    refactor(infinite): refactor modules (WIP)

--- src/Infinite.js
+++ src/Infinite.js
@@ -1,85 +1,27 @@
 import Component from "@egjs/component";
 import ItemManager from "./ItemManager";
+import ItemRenderer from "./ItemRenderer";
 import ImageLoaded from "./ImageLoaded";
-// import GridLayout from "./layouts/GridLayout";
-// import FrameLayout from "./layouts/FrameLayout";
+
+import GridLayout from "./layouts/GridLayout";
+import FrameLayout from "./layouts/FrameLayout";
+import FacebookLayout from "./layouts/FacebookLayout";
+import PackingLayout from "./layouts/PackingLayout";
 import JustifiedLayout from "./layouts/JustifiedLayout";
+
 import {
 	APPEND,
 	PREPEND,
-	DUMMY_POSITION,
-	MULTI,
 	CACHE,
 	NO_CACHE,
 } from "./consts";
-import {
-	$,
+// import {
+// 	$,
+// 	addEvent,
+// 	removeEvent,
+// } from "./utils";
 
-} from "./utils";
-
-export class ItemRenderer {
-	constructor(element) {
-		this._el = $(element);
-		this._el.style.position = "relative";
-	}
-	static renderItem(item, styles) {
-		if (item.el) {
-			const elStyle = item.el.style;
-
-			// for debugging
-			item.el.setAttribute("data-groupkey", item.groupKey);
-			elStyle.position = "absolute";
-			["left", "top", "width", "height"].forEach(p => {
-				(p in styles) && (elStyle[p] = `${styles[p]}px`);
-			});
-		}
-	}
-	static removeItems(items) {
-		items.forEach(item => {
-			if (item.el) {
-				item.el.parentNode.removeChild(item.el);
-				item.el = null;
-			}
-		});
-	}
-	append(items) {
-		this._insert(items, APPEND, {
-			top: DUMMY_POSITION,
-		});
-	}
-	prepend(items) {
-		this._insert(items, PREPEND, {
-			top: DUMMY_POSITION,
-		});
-	}
-	createAndInsert(items, isAppend) {
-		const elements = $(items.reduce((acc, v) => acc.concat(v.content), []).join(
-			""), MULTI);
-		const itemsWithElement = items.map((item, index) => {
-			item.el = elements[index];
-			return item;
-		});
-
-		ItemRenderer.render(itemsWithElement);
-		this._insert(itemsWithElement, isAppend);
-	}
-	_insert(items, isAppend, styles) {
-		const df = document.createDocumentFragment();
-
-		items.forEach(item => {
-			styles && ItemRenderer.renderItem(item, styles);
-
-			isAppend ? df.appendChild(item.el) : df.insertBefore(item.el, df.firstChild);
-		});
-		isAppend ? this._el.appendChild(df) : this._el.insertBefore(df, this._el.firstChild);
-	}
-	static render(items) {
-		items.forEach(item => {
-			ItemRenderer.renderItem(item, item.rect);
-		});
-	}
-}
-
+export {JustifiedLayout, GridLayout, FrameLayout, FacebookLayout, PackingLayout};
 
 export class Infinite extends Component {
 	constructor(element, options) {
@@ -87,41 +29,50 @@ export class Infinite extends Component {
 		Object.assign(this.options = {
 			itemSelector: "*",
 			count: 60,
+			widthAttr: "data-width",
+			heightAttr: "data-height",
 		}, options);
 		this._startCursor = -1;
 		this._endCursor = -1;
-		// @todo
-		this._layout = new JustifiedLayout({
-			direction: "vertical",
-			minSize: 100,
-		});
-		// this._layout = new GridLayout({
-		// 	direction: "vertical",
-		// 	align: "start",
-		// 	// minSize: 100,
-		// });
-		// this._layout = new FrameLayout({
-		// 	direction: "vertical",
-		// 	margin: 20,
-		// 	minSize: 100,
-		// 	frame: [
-		// 		[1, 0, 1, 0, 1],
-		// 		[2, 2, 2, 3, 3],
-		// 		[2, 2, 2, 3, 3],
-		// 		[0, 4, 0, 4, 0],
-		// 	],
-		// 	frameFill: true,
-		// });
-		this._layout.setViewport(471, 1320);
-		this._checkImageloaded = new ImageLoaded();
-		this._itemManager = new ItemManager();
+		this._items = new ItemManager();
 		this._renderer = new ItemRenderer(element);
 	}
-	append(elements, groupKey) { // 사용자가 append를 호출
-		this._insert(elements, groupKey, APPEND);
+	// called by user
+	append(elements, groupKey) {
+		this._layout && this._insert(elements, groupKey, APPEND);
 	}
-	prepend(elements, groupKey) { // 사용자가 prepend를 호출
-		this._insert(elements, groupKey, PREPEND);
+	// called by user
+	prepend(elements, groupKey) {
+		this._layout && this._insert(elements, groupKey, PREPEND);
+	}
+	setLayout(layout) {
+		this._layout = layout;
+	}
+	setViewport(width, height) {
+		this._layout && this._layout.setViewport(width, height);
+	}
+	getVisibleItems() {
+		return this._items.getItems(this._startCursor, this._endCursor);
+	}
+	getItems(isAppend) {
+		let items = [];
+		const size = this._items.size();
+
+		// from cache
+		if (size > 0 && this._startCursor !== -1 && this._endCursor !== -1) {
+			if (isAppend && size > this._endCursor + 1) {
+				items = this._items.getItems(this._endCursor + 1);
+			} else if (!isAppend && this._startCursor > 0) {
+				items = this._items.getItems(this._startCursor - 1);
+			}
+		}
+		return items;
+	}
+	clear() {
+		this._renderer.clear();
+		this._items.clear();
+		this._startCursor = -1;
+		this._endCursor = -1;
 	}
 	_insert(elements, groupKey, isAppend) {
 		const key = typeof groupKey === "undefined" ? (new Date().getTime() + Math.floor(
@@ -145,15 +96,11 @@ export class Infinite extends Component {
 		let diff;
 
 		while ((diff = this.getVisibleItems().length + baseCount) > 0) {
-			// @todo range가 넘어가는 경우에대한 별도의 처리 및 테스트가 필요함.
-			let toRemoveItems;
+			const toRemoveItems = this._items.getItems(isAppend ? this._startCursor : this._endCursor);
 
-			console.log(diff, "recyle이 필요함, DOM 삭제");
 			if (isAppend) {
-				toRemoveItems = this._itemManager.getItems(this._startCursor);
 				this._startCursor++;
 			} else {
-				toRemoveItems = this._itemManager.getItems(this._endCursor);
 				this._endCursor--;
 			}
 			// recycle
@@ -174,31 +121,12 @@ export class Infinite extends Component {
 		}
 		console.warn("_updateCursor [", this._startCursor, this._endCursor, "]");
 	}
-	_insertItemManager(layouted, isAppend) {
+	_insertItems(layouted, isAppend) {
 		layouted.groupKey = layouted.items[0].groupKey;
-		this._itemManager[isAppend ? "append" : "prepend"](layouted);
+		this._items[isAppend ? "append" : "prepend"](layouted);
 	}
-	getVisibleItems() {
-		return this._itemManager.getItems(this._startCursor, this._endCursor);
-	}
-	getItems(isAppend) {
-		let items = [];
-		const size = this._itemManager.size();
-
-		// 데이터가 존재한다.
-		if (size > 0 && this._startCursor !== -1 && this._endCursor !== -1) {
-			if (isAppend && size > this._endCursor + 1) {
-				console.log("데이터가 있다");
-				items = this._itemManager.getItems(this._endCursor + 1);
-			} else if (!isAppend && this._startCursor > 0) {
-				console.log("데이터가 있다");
-				items = this._itemManager.getItems(this._startCursor - 1);
-			}
-		}
-		return items;
-	}
-
-	requestAppend() { // visible이 호출
+	// called by visible
+	_requestAppend() {
 		const items = this.getItems(APPEND);
 
 		if (items.length) {
@@ -208,7 +136,8 @@ export class Infinite extends Component {
 			this.trigger("append");
 		}
 	}
-	requestPrepend() { // visible이 호출
+	// called by visible
+	_requestPrepend() {
 		const items = this.getItems(PREPEND);
 
 		if (items.length) {
@@ -227,22 +156,22 @@ export class Infinite extends Component {
 
 			this._renderer[method](items);
 			// check image sizes after elements are attated on DOM
-			this._checkImageloaded.check(items.map(item => item.el), () => {
-				const layouted = this._layout[method](
-					ItemManager.updateSize(items),
-					this._itemManager.getOutline(
-						isAppend ? this._endCursor : this._startCursor,
-						isAppend)
-				);
+			ImageLoaded.check(items.map(item => item.el), {
+				widthAttr: this.options.widthAttr,
+				heightAttr: this.options.heightAttr,
+				callback: () => {
+					const layouted = this._layout[method](
+						ItemManager.updateSize(items),
+						this._items.getOutline(
+							isAppend ? this._endCursor : this._startCursor,
+							isAppend)
+					);
 
-				this._insertItemManager(layouted, isAppend);
-				this._updateCursor(isAppend);
-				ItemRenderer.render(layouted.items);
+					this._insertItems(layouted, isAppend);
+					this._updateCursor(isAppend);
+					ItemRenderer.render(layouted.items);
+				},
 			});
 		}
-	}
-
-	setLayout(layout) {
-		this._layout = layout;
 	}
 }
--- test/manual/js/infinite.js
+++ test/manual/js/infinite.js
@@ -26,18 +26,144 @@ var infinite = new eg.InfiniteGrid.Infinite("#infinite")
   });
 
 
+
+		// this._layout = new FrameLayout({
+		// 	direction: "vertical",
+		// 	margin: 20,
+		// 	frame: [
+		// 		["C", "", "A", "A", "A"],
+		// 		["C", "B", "B", "E", "E"],
+		// 		["C", "D", "D", "E", "E"],
+		// 		["F", "F", "", "H", "H"],
+		// 	],
+		// });
+
 // 테스트를 위한 코드
 var gui = new dat.GUI();
 var guioption = {
 	requestAppend: function() {
-		infinite.requestAppend();
+		infinite._requestAppend();
 	},
 	requestPrepend: function() {
-    infinite.requestPrepend();
-	}
+    infinite._requestPrepend();
+  },
+  clear: function() {
+    infinite.clear();
+  },
+  layout: function() {
+    infinite.layout();
+  },
+  width: 800,
+  height: 1000,
+  direction: "vertical",
+  margin: 10,
 };
 
-var fold = gui.addFolder("Data");
-fold.add(guioption, "requestAppend");
-fold.add(guioption, "requestPrepend");
-fold.open();
+function setViewport() {
+  infinite.setViewport(guioption.width, guioption.height);
+}
+var justified = {
+  minSize: 100,
+  maxSize: 200,
+  set: function() {
+    infinite.setLayout(new eg.InfiniteGrid.JustifiedLayout({
+      direction: guioption.direction,
+      margin: guioption.margin,
+      minSize: justified.minSize,
+      maxSize: justified.maxSize
+    }));
+    setViewport();
+  }
+};
+var grid = {
+  align: "start",
+  set: function() {
+    infinite.setLayout(new eg.InfiniteGrid.GridLayout({
+      direction: guioption.direction,
+      margin: guioption.margin,
+      align: grid.align
+    }));
+    setViewport();
+  }
+};
+var packing = {
+  aspectRatio: 1.2,
+  set: function() {
+    infinite.setLayout(new eg.InfiniteGrid.PackingLayout({
+      direction: guioption.direction,
+      margin: guioption.margin,
+      aspectRatio: packing.aspectRatio
+    }));
+    setViewport();
+  }
+};
+var facebook = {
+  column: 1,
+  set: function() {
+    infinite.setLayout(new eg.InfiniteGrid.FacebookLayout({
+      direction: guioption.direction,
+      margin: guioption.margin,
+      column: facebook.column
+    }));
+    setViewport();
+  }
+};
+var frame = {
+  frame: [[]],
+  set: function() {
+    infinite.setLayout(new eg.InfiniteGrid.FrameLayout({
+      direction: guioption.direction,
+      margin: guioption.margin,
+      frame: facebook.column
+    }));
+    setViewport();
+  }
+};
+
+var fold0 = gui.addFolder("Action");
+fold0.add(guioption, "requestPrepend");
+fold0.add(guioption, "requestAppend");
+fold0.add(guioption, "clear");
+fold0.add(guioption, "layout");
+fold0.open();
+
+var fold1 = gui.addFolder("Data");
+fold1.add(guioption, "width", 800, 1600).onFinishChange(() => setViewport());
+fold1.add(guioption, "height", 1000, 2000).onFinishChange(() => setViewport());
+fold1.add(guioption, "direction", ["vertical", "horizontal"]).onFinishChange(() => infinite.clear());
+fold1.add(guioption, "margin", 10, 50).onFinishChange(() => infinite.clear());
+fold1.open();
+
+var fold2 = gui.addFolder("JustifiedLayout");
+fold2.add(justified, "minSize", 10, 300).onFinishChange(() => justified.set());
+fold2.add(justified, "maxSize", 100, 2000).onFinishChange(() => justified.set());
+fold2.add(justified, "set");
+fold2.open();
+
+var fold3 = gui.addFolder("GridLayout");
+fold3.add(grid, "align", ["start", "center", "end", "justify"]).onFinishChange(() => grid.set());
+fold3.add(grid, "set");
+fold3.open();
+
+var fold4 = gui.addFolder("PackingLayout");
+fold4.add(packing, "aspectRatio", 1, 3).onFinishChange(() => packing.set());
+fold4.add(packing, "set");
+fold4.open();
+
+var fold5 = gui.addFolder("FacebookLayout");
+fold5.add(facebook, "column", 1, 10).onFinishChange(() => facebook.set());
+fold5.add(facebook, "set");
+fold5.open();
+
+var fold6 = gui.addFolder("FrameLayout");
+// fold5.add(facebook, "frame", 1, 10).onFinishChange(() => facebook.set());
+fold6.add(frame, "set");
+fold6.open();
+
+
+// frame: [
+//   // 		["C", "", "A", "A", "A"],
+//   // 		["C", "B", "B", "E", "E"],
+//   // 		["C", "D", "D", "E", "E"],
+//   // 		["F", "F", "", "H", "H"],
+//   // 	],
