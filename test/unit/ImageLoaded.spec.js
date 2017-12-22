a63815113522b825d7923c3afa726c5bad2b27a6
Younkue <younkue.choi@navercorp.com>
2017-12-22 16:39:32 +0900

    test(ImageLoaded): increase coverage

--- test/unit/ImageLoaded.spec.js
+++ test/unit/ImageLoaded.spec.js
@@ -34,11 +34,61 @@ describe("ImageLoaded Test", function() {
 
         const complete = sinon.spy(e => {
             expect(error.calledOnce).to.be.false;
+            expect(img.complete).to.be.true;
             done();
         });
         const error = sinon.spy(e => {
             
         })
         ImageLoaded.check([img], "data-", complete, error);
+    });
+    it(`should check image included size sucess`, done => {
+        const img = $(`<img src="/base/test/unit/image/3.jpg" data-width="400" data-height="500" />`);
+        this.container.appendChild(img);
+
+        const complete = sinon.spy(e => {
+        });
+        const error = sinon.spy(e => {
+            
+        })
+        ImageLoaded.check([img], "data-", complete, error);
+        setTimeout(() => {
+            expect(error.calledOnce).to.be.false;
+            expect(complete.calledOnce).to.be.true;
+            done();
+        }, 5);
+    });
+    it(`should check image included other prefix size sucess`, done => {
+        const img = $(`<img src="/base/test/unit/image/3.jpg" width="400" height="500" />`);
+        this.container.appendChild(img);
+
+        const complete = sinon.spy(e => {
+        });
+        const error = sinon.spy(e => {
+            
+        })
+        ImageLoaded.check([img], "", complete, error);
+        setTimeout(() => {
+            expect(error.calledOnce).to.be.false;
+            expect(complete.calledOnce).to.be.true;
+            done();
+        }, 5);
+    });
+    it(`should check item included image`, done => {
+        const div = $(`<div><img src="/base/test/unit/image/3.jpg"/><img src="/base/test/unit/image/3.jpg"/></div>`);
+        this.container.appendChild(div);
+
+        const complete = sinon.spy(e => {
+            div.querySelectorAll("img").forEach(element => {
+                expect(element.complete).to.be.true;
+            });
+            expect(error.calledOnce).to.be.false;
+            expect(complete.calledOnce).to.be.true;
+            done();
+        });
+        const error = sinon.spy(e => {
+            
+        })
+        ImageLoaded.check([div], "data-", complete, error);
     });
 });
