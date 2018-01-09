9250f84d27c8d028a72ac7f34ea868e2c88dbb24
Younkue <younkue.choi@navercorp.com>
2018-01-09 15:22:33 +0900

    demo(InfiniteGrid): add media selector

--- demo/assets/js/search2.js
+++ demo/assets/js/search2.js
@@ -15,89 +15,52 @@ function getItems(no, length) {
 	return arr;
 }
 
-var REQUEST_CHANGE = 0;
-var REQUEST_APPEND = 1;
-var REQUEST_PREPEND = 2;
-var REQUEST_APPEND_ANIMATE = 3;
-var REQUEST_PREPEND_ANIMATE = 4;
 var groups = {};
-var deltaPull = 10;
 var container = document.querySelector(".container");
 var contents = document.querySelector(".contents");
-var ig = new eg.InfiniteGrid(contents);
-var prevScrollPosition = 0;
-var pullScrollPosition = 0;
-var isRequestPull = REQUEST_PREPEND;
-var isLoading = false;
+var ig = new eg.InfiniteGrid(contents, {
+	isOverflowScroll: true
+});
 
 ig.setLayout(eg.InfiniteGrid.JustifiedLayout, {
 	minSize: 100,
 	maxSize: 300,
 	margin: 10,
 });
-ig.on({
-	"change": function (e) {
-		var scrollPosition = e.orgScrollPos;
 
-		prevScrollPosition = scrollPosition;
-
-		if (isLoading || isRequestPull >= REQUEST_APPEND_ANIMATE ||
-			(isRequestPull >= REQUEST_APPEND_ANIMATE) ||
-			(isRequestPull > 0 && Math.abs(scrollPosition - pullScrollPosition) < deltaPull)) {
-			return;
-		}
-		isRequestPull = REQUEST_CHANGE;
-	},
-	"prepend": function (e) {
-		var groupKeys = ig.getGroupKeys(true);
-		var groupKey = (groupKeys[0] || 0) - 1;
-
-		if (!(groupKey in groups)) {
-			isRequestPull = REQUEST_PREPEND;
-			pullScrollPosition = prevScrollPosition;
-			return;
-		}
-		ig.prepend(groups[groupKey], groupKey);
-	},
-	"append": function(e) {
-		var groupKeys = ig.getGroupKeys(true);
-		var groupKey = (groupKeys[0] || 0) - 1;
-
-		if (!(groupKey in groups)) {
-			isRequestPull = REQUEST_APPEND;
-			pullScrollPosition = prevScrollPosition;
-			return;
-		}
-		ig.append(groups[groupKey], groupKey);
-	},
-	"layoutComplete": function (e) {
-		if (ig.getGroupKeys().length === 1) {
-			return;
-		}
-		if (isRequestPull === REQUEST_APPEND) {
-			contents.scrollTop += 20;
-			isRequestPull = REQUEST_APPEND_ANIMATE;
-		} else if (isRequestPull === REQUEST_PREPEND) {
-			contents.scrollTop -= 20;
-			isRequestPull = REQUEST_PREPEND_ANIMATE;
-		}
-	}
-});
-groups[0] = getItems(0, 60, true);
-ig.append(groups[0], 0);
-
-
+container.insertAdjacentHTML("beforeend", '<div id="prepend"></div><div id="append"></div>');
+var prepend = document.getElementById("prepend");
+var append = document.getElementById("append");
 var axes = new eg.Axes({
 	scroll: {
 		range: [0, 0],
 		bounce: 100
 	}
 });
+var isTouch = false;
+var isLoading = true;
 
-container.insertAdjacentHTML("beforeend", '<div id="prepend"></div><div id="append"></div>');
-var prepend = document.getElementById("prepend");
-var append = document.getElementById("append");
-var isLoading = false;
+ig.on({
+	"change": function (e) {
+		if (isLoading) {
+			return;
+		}
+		if (!isTouch) {
+			axes.setTo({scroll: e.scrollPos}, 0);
+		}
+	},
+	"layoutComplete": function (e) {
+		axes.axis.scroll.range[1] = contents.scrollHeight - contents.clientHeight;
+		axes.setTo({ scroll: e.scrollPos }, 0);
+		if (isLoading) {
+			isLoading = false;
+			container.className = container.className.replace(/pull/g, "");
+		}
+	},
+});
+groups[0] = getItems(0, 30, true);
+ig.append(groups[0], 0);
+
 
 function requestInsert(isAppend) {
 	container.className = container.className + " pull";
@@ -108,75 +71,75 @@ function requestInsert(isAppend) {
 
 		groups[groupKey] = getItems(groupKey, 30, isAppend);
 		ig[isAppend ? "append" : "prepend"](groups[groupKey], groupKey);
-		isLoading = false;
-		axes.setTo({ scroll: 0 }, 500);
-		container.className = container.className.replace(/pull/g, "");
 	}, 1000);
 }
+
+var startBouncing = false;
+
 axes.on({
 	"change": function (e) {
+		if (e.holding && isLoading) {
+			return;
+		}
 		var pos = e.pos;
-		if (!isRequestPull || isLoading || !axes.isBounceArea()) {
-			return;
-		}
 		var scroll = pos.scroll;
-		var height = Math.abs(scroll);
+		var maxRange = axes.axis.scroll.range[1];
+
+		isTouch = !!e.inputEvent;
+
+		if (!axes.isBounceArea()) {
+			append.style.height = "0px";
+			prepend.style.height = "0px";
+			contents.style.transform = "";
+			if (isTouch) {
+				contents.scrollTop = scroll;
+			}
+			return;
+		}
+		var bounce = (scroll < 0 ? scroll : scroll - maxRange);
+		var weight = Math.abs(bounce);
 		var isAppend = scroll > 0;
-
-
-		if (isAppend &&
-			(isRequestPull !== REQUEST_APPEND && isRequestPull !== REQUEST_APPEND_ANIMATE)) {
-			return;
-		}
-		if (!isAppend &&
-			(isRequestPull !== REQUEST_PREPEND && isRequestPull !== REQUEST_PREPEND_ANIMATE)) {
-			return;
-		}
-
-		contents.style.transition = "";
-		contents.style.transform = "translateY(" + (-scroll) + "px)";
-
 		var element = isAppend ? append : prepend;
 
-		element.style.height = height + "px";
-		if (height < 80) {
-			element.innerHTML = "Pull to " + (isAppend ? "append" : "prepend");
-		} else {
-			element.innerHTML = "Release to " + (isAppend ? "append" : "prepend");
-		}
+		contents.style.transform = "translateY(" + (-bounce) + "px)";
+		element.style.height = weight + "px";
+		element.innerHTML = (weight > 80 ? "Release to " : "Pull to ") + (isAppend ? "append" : "prepend");
 	},
-	"release": function (e) {
-		var depaPos = e.depaPos;
-
-		if (!isRequestPull || isLoading || !axes.isBounceArea() || !depaPos) {
+	"release": function(e) {
+		if (isLoading || !axes.isBounceArea()) {
 			return;
 		}
-		var scroll = depaPos.scroll;
-		var height = Math.abs(scroll);
+		var scroll = e.depaPos.scroll;
+		var maxRange = axes.axis.scroll.range[1];
 		var isAppend = scroll > 0;
+		var weight = Math.abs(scroll < 0 ? scroll : maxRange - scroll);
 
-		if ((isRequestPull === REQUEST_PREPEND && isAppend) ||
-			(isRequestPull === REQUEST_APPEND && !isAppend)) {
-			return;
+		if (weight > 80) {
+			isLoading = true;
+			(isAppend ? append : prepend).innerHTML = "Loading...";
+			requestInsert(isAppend);
 		}
-		if (height < 80) {
-			return;
-		}
-		isLoading = true;
-		(isAppend ? append : prepend).innerHTML = "Loading...";
-		requestInsert(isAppend);
 	},
 	"animationStart": function (e) {
 		if (isLoading) {
 			e.stop();
+			return;
+		}
+		isTouch = true;
+
+		if (!axes.isBounceArea()) {
+			var maxRange = axes.axis.scroll.range[1];
+
+			if (e.destPos.scroll < 0) {
+				e.setTo({scroll: 0}, e.duration);
+			} else if (e.destPos.scroll > maxRange) {
+				e.setTo({scroll: maxRange}, e.duration);
+			}
 		}
 	},
 	"animationEnd": function (e) {
-		if (isRequestPull >= REQUEST_APPEND_ANIMATE) {
-			isRequestPull = REQUEST_CHANGE;
-		}
-	},
+		isTouch = false;
+	}
 });
 
-
-axes.connect(["", "scroll"], new eg.Axes.PanInput(container, { scale: [0, -1] }));
+axes.connect(["", "scroll"], new eg.Axes.PanInput(container, {scale: [0, -1]}));
