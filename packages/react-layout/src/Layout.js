cfeeef4c5705dedfe1bdb7b37c159a04346064cc
Younkue <younkue.choi@navercorp.com>
2018-01-27 18:59:41 +0900

    feat(react): add react-layout README

---
+++ packages/react-layout/src/Layout.js
@@ -0,0 +1,12 @@
+import {Component} from "react";
+import {ImageLoaded} from "@egjs/infinitegrid";
+
+class Layout extends Component {
+    ImageLoaded.check({
+        success: e => {
+            
+        }
+        end: 
+    })
+
+}
