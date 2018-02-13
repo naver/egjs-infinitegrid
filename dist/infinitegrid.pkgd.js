7e328ef4efb90c8d969a8dde29e4af690cbb9810
Younkue <younkue.choi@navercorp.com>
2018-02-13 18:42:51 +0900

    chore(release): Release 3.2.2

--- dist/infinitegrid.pkgd.js
+++ dist/infinitegrid.pkgd.js
@@ -5,7 +5,7 @@
  * @egjs/infinitegrid JavaScript library
  * https://github.com/naver/egjs-infinitegrid
  * 
- * @version 3.2.2-rc
+ * @version 3.2.2
  * 
  * All-in-one packaged file for ease use of '@egjs/infinitegrid' with below dependencies.
  * NOTE: This is not an official distribution file and is only for user convenience.
@@ -2567,7 +2567,7 @@ var InfiniteGrid = function (_Component) {
 	return InfiniteGrid;
 }(_component2["default"]);
 
-InfiniteGrid.VERSION = "3.2.2-rc";
+InfiniteGrid.VERSION = "3.2.2";
 
 exports["default"] = InfiniteGrid;
 module.exports = exports["default"];
