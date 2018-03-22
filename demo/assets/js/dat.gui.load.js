c4fba8c3ea386af8218339db9149bb93462e22be
Younkue <younkue.choi@navercorp.com>
2018-03-22 18:44:49 +0900

    demo(InfiniteGrid): fix gui demo

--- demo/assets/js/dat.gui.load.js
+++ demo/assets/js/dat.gui.load.js
@@ -1,22 +1,28 @@
 function loadGUI(text) {
 	window.onload = function() {
 		var gui = new dat.GUI();
-		gui.width = 140;
+		gui.width = 180;
 
 		var change = function() {
 			text.change(text);
 		}
 		for (var name in text) {
+
+			var item;
 			if (typeof text[name] === "function") {
 				continue;
 			} else if (typeof text[name] === "object") {
-				var apply = [text, name].concat(text[name].slice(1));
-
-				text[name] = text[name][0];
-				gui.add.apply(gui, apply).onFinishChange(change);
+				var contents = text[name];
+				text[name] = contents[0];
+				if (typeof contents[1] === "object") {
+					item = gui.add(text, name, contents[1]);
+				} else {
+					item = gui.add(text, name, contents[1], contents[2], contents[3] || 1);
+				}
 			} else {
-				gui.add(text, name).onFinishChange(change);
+				item = gui.add(text, name);
 			}
+			item.onFinishChange(change);
 		}
 	};
 }
