396a01636cf8de4fac1525d30bd566d9705b4100
Younkue <younkue.choi@navercorp.com>
2018-06-12 11:57:20 +0900

    demo(react-infinitegrid): add youtube demo

--- packages/react-infinitegrid/webpack.config.js
+++ packages/react-infinitegrid/webpack.config.js
@@ -35,6 +35,13 @@ module.exports = {
 				test: /\.js$/,
 				loader: "babel-loader",
 			},
+			{
+				test: /\.css$/,
+				use: [
+					{loader: "style-loader"},
+					{loader: "css-loader"},
+				],
+			},
 		],
 	},
 	plugins: [
