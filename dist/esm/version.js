ed191485a9fdd9bb847c1106e308237216f5391d
Younkue <younkue.choi@navercorp.com>
2018-10-04 18:20:17 +0900

    chore(release): Release 3.4.4

---
+++ dist/esm/version.js
@@ -0,0 +1,11 @@
+/**
+ * Version info string
+ * @ko 버전정보 문자열
+ * @name VERSION
+ * @static
+ * @type {String}
+ * @example
+ * eg.InfiniteGrid.VERSION;  // ex) 3.3.3
+ * @memberof eg.InfiniteGrid
+ */
+module.exports = "3.4.4";
