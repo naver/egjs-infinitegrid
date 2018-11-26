a5c7601ec007f028b42c7e14e8e1ed25824b9366
Daybrush <younkue.choi@navercorp.com>
2018-11-26 16:50:46 +0900

    fix(scripts): fix auto caculate size script

--- config/readme.js
+++ config/readme.js
@@ -10,3 +10,13 @@ fs.writeFileSync("./README.md", data.replace(/\|\s*\[(infinitegrid(?:\.[a-z]+)?(
 
 	return all.replace(sizearea, `(${size / 10}kb)`);
 }));
+
+
+const egjs = fs.readFileSync("./demo/_data/egjs.yml", "utf8");
+
+fs.writeFileSync("./demo/_data/egjs.yml", egjs.replace(/file:\s(\S*)\n\s\ssize:\s(\S+)/g, (all, filename, sizearea) => {
+	const stats = fs.statSync(`./dist/${filename}`);
+	const size = Math.floor(stats.size / 102.4);
+
+	return all.replace(sizearea, `${size / 10}kb`);
+}));
