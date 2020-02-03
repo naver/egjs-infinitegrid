2c45ad9f1bae449d5d62a05b7fcfd57f521c26da
daybrush <daybrush@gmail.com>
2020-02-03 15:16:29 +0900

    demo: support for cid's cookie

--- storybook/stories/ga.ts
+++ storybook/stories/ga.ts
@@ -1,9 +1,37 @@
 
 const tid = "UA-70842526-18";
-const cid = (Math.random() * Math.pow(10, 20)) / Math.pow(10, 10);
 const z = Math.floor(Math.random() * 10000000);
+
+function random() {
+    return Math.floor((Math.random() * Math.pow(10, 10)));
+}
+function setCookie(name, value) {
+    const date = new Date();
+
+    date.setTime(date.getTime() + 3600 * 1000);
+    document.cookie = `${name}=${value};expires=${date.toUTCString()};path=/`;
+}
+function getCookie(name) {
+    const cookieName = `${name}=`;
+    const cookies = document.cookie.split(";");
+    const length = cookies.length;
+
+    for (let i = 0; i < length; ++i) {
+        const text = cookies[i].trim();
+
+        if (text.indexOf(cookieName) === 0) {
+            return text.substring(cookieName.length);
+        }
+    }
+    return "";
+}
+
 export function ga(title: string) {
   try {
+    const _ga = getCookie("_ga") || `GA1.3.${random()}.${random()}`;
+
+    setCookie("_ga", _ga);
+    const cid = _ga.replace(/GA\d+\.\d+\./g, "");
     const innerWidth = window.innerWidth;
     const innerHeight = window.innerHeight;
     const screen = window.screen || { width: innerWidth, height: innerHeight };
