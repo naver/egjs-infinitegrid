b9ecb270c6e59fcaec57fe5e57e99dab2258fc9d
daybrush <daybrush@gmail.com>
2021-01-04 12:23:55 +0900

    skip: apply review

--- packages/svelte-infinitegrid/babel.config.js
+++ packages/svelte-infinitegrid/babel.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  presets: [
-    [
-      "@babel/preset-env",
-      {
-        modules: "cjs",  
-      }
-    ]
-  ]
+	presets: [
+		[
+			"@babel/preset-env",
+			{
+				modules: "cjs",
+			},
+		],
+	],
 };
