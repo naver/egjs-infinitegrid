0318d416acb63611890dc752815386af7497ca19
daybrush <daybrush@gmail.com>
2021-02-03 17:27:50 +0900

    chore(release): Release 3.8.0

--- packages/react-infinitegrid/src/demo/App.tsx
+++ packages/react-infinitegrid/src/demo/App.tsx
@@ -30,7 +30,7 @@ class App extends React.Component<{}, {
     super(prop);
     this.state = {
       list: [
-        // ...this.loadItems("1", 10).slice(1),
+        // ...this.loadItems("1", 10),
         // ...this.loadItems("2", 10),
       ],
     };
