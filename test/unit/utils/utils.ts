31a6de67d29e1a4593aa5bf791749a269ce20783
Daybrush <daybrush@gmail.com>
2021-05-14 13:47:11 +0900

    test: test ScrollManager

---
+++ test/unit/utils/utils.ts
@@ -0,0 +1,45 @@
+import Component from "@egjs/component";
+
+export function sandbox(obj: object | string, prop?: object): HTMLElement {
+  const tmp = document.createElement("div");
+  tmp.className = "_tempSandbox_";
+  if (typeof obj === "string") {
+    tmp.id = obj;
+  } else {
+    tmp.id = "sandbox";
+  }
+
+  if (typeof obj === "object" || typeof prop === "object") {
+    const attrs = typeof prop === "object" ? prop : obj;
+    for (const p in attrs as object) {
+      if (/class|className/.test(p)) {
+        tmp.setAttribute(p, attrs[p] + " _tempSandbox_");
+      } else {
+        tmp.setAttribute(p, attrs[p]);
+      }
+    }
+  }
+  document.body.appendChild(tmp);
+  return tmp;
+}
+
+export function cleanup() {
+  const elements: HTMLElement[] = [].slice.call(document.querySelectorAll("._tempSandbox_"));
+  elements.forEach((v) => {
+    v.parentNode!.removeChild(v);
+  });
+}
+
+export function waitFor(delay: number): Promise<void> {
+  return new Promise((resolve) => {
+    setTimeout(resolve, delay);
+  });
+}
+
+export function waitEvent(component: Component<any>, eventName: string): Promise<any> {
+  return new Promise((resolve) => {
+    component.once(eventName, (e) => {
+      resolve(e);
+    });
+  });
+}
