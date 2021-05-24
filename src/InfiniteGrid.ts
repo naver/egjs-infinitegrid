3e7f157d17f37ba1779e7821f377c8067ddad84d
Daybrush (Younkue Choi) <daybrush@gmail.com>
2021-05-24 11:54:33 +0900

    feat: add infinite (#400)
    * feat: add infinite
    
    * test: test infinite
    
    * test: test infinite sync

--- src/InfiniteGrid.ts
+++ src/InfiniteGrid.ts
@@ -1,4 +1,5 @@
-import { ContainerManager } from "@egjs/grid";
+import { ContainerManager, GridItem } from "@egjs/grid";
+import { Infinite } from "./Infinite";
 import { ScrollManager } from "./ScrollManager";
 import { isString } from "./utils";
 
@@ -6,7 +7,7 @@ export class InfiniteGrid {
   protected wrapperElement: HTMLElement;
   protected scrollManager: ScrollManager;
   protected containerManager: ContainerManager;
-
+  protected infinite: Infinite;
   constructor(wrapper: HTMLElement | string) {
     // options.container === false, wrapper = container, scrollContainer = document.body
     // options.container === true, wrapper = scrollContainer, container = wrapper's child
@@ -21,8 +22,86 @@ export class InfiniteGrid {
       horizontal: false,
     });
 
+    const infinite = new Infinite({
+      useRecyle: false,
+    }).on({
+      "change": this._onChange,
+      "requestAppend": this._onRequestAppend,
+      "requestPrepend": this._onRequestPrepend,
+    });
+
     this.wrapperElement = wrapperElement;
     this.scrollManager = scrollManager;
     this.containerManager = containerManager;
+    this.infinite = infinite;
+  }
+  public syncItems() {
+    // item sync
+    // const {
+    //   added,
+    //   removed,
+    //   ordered,
+    // } = this.groupManager.syncItems([]);
+    // infinite sync
+    // const prevVisbleItems = this.getVisibleItems();
+    this.infinite.sync([]);
+    // const nextVisbleItems = this.getVisibleItems();
+    // const container = this.scrollManager.getContainer();
+    // const {
+    //   added,
+    //   ordered,
+    //   removed,
+    // } = diff(prevVisbleItems, nextVisbleItems, (item) => item.key);
+
+    // if (added.length || ordered.length) {
+    //   container.innerHTML = "";
+
+    //   const fragment = document.createDocumentFragment();
+    //   nextVisbleItems.forEach((item) => {
+    //     if (!item.element) {
+    //       // create element
+    //     }
+    //     fragment.appendChild(item.element!);
+    //   });
+    //   container.appendChild(fragment);
+    // } else if (removed.length) {
+    //   removed.forEach((index) => {
+    //     const item = nextVisbleItems[index];
+
+
+    //     if (item.mountState === MOUNT_STATE.MOUNTED) {
+    //       const element = item.element;
+
+    //       container.removeChild(element!);
+    //     }
+    //   });
+    // }
+    // save sync info
+    // this.status.isChange = isChange;
+  }
+  public syncElements() {
+    return;
+  }
+  public setCursors(startCursor: number, endCursor: number) {
+    this.infinite.setCursors(startCursor, endCursor);
+  }
+  public getVisibleItems(): GridItem[] {
+    return [];
+    // return this.getVisibleGroups().map((group) => group.getItems());
+  }
+  public getVisibleGroups(): [] {
+    return [];
+    // return this.infinite.getVisibleItems().map(item => {
+    //   // return this.groupManager.getItem(item.key);
+    // });
+  }
+  private _onChange = () => {
+    //
+  }
+  private _onRequestAppend = () => {
+    //
+  }
+  private _onRequestPrepend = () => {
+    //
   }
 }
