4427b8f9164e0c2bb18b21bb2ce556dfa6e77e6e
Daybrush <daybrush@gmail.com>
2021-06-07 15:46:57 +0900

    feat: add syncItems in GroupManager

--- src/GroupManager.ts
+++ src/GroupManager.ts
@@ -1,20 +1,51 @@
 import Grid, { GridItem } from "@egjs/grid";
-import { diff } from "@egjs/list-differ";
 
+export interface GroupManagerOptions {
+  gridContructor: new (container, gridOptions) => Grid;
+}
 export interface Group {
-  key: number | string;
+  groupKey: number | string;
   grid: Grid;
 }
 export interface GroupManagerItem {
-  groupkey: string | number;
+  groupKey: string | number;
   key: string | number;
-  item?: null;
+  item?: GridItem | null;
+}
+export interface GroupManagerGroup {
+  groupKey: string | number;
+  items: GroupManagerItem[];
+}
+
+export function categorize(items: GroupManagerItem[]): GroupManagerGroup[] {
+  const groups: GroupManagerGroup[] = [];
+
+  const groupKeys: { [key: string]: GroupManagerGroup } = {};
+
+  items.forEach((item) => {
+    const { groupKey } = item;
+    let group = groupKeys[groupKey];
+
+    if (!group) {
+      group = {
+        groupKey,
+        items: [],
+      };
+      groupKeys[groupKey] = group;
+      groups.push(group);
+    }
+
+    group.items.push(item);
+  });
+  return groups;
 }
 
 export class GroupManager {
   protected groups: Group[] = [];
+  protected groupKeys: Record<string | number, Group> = {};
   protected items: GroupManagerItem[] = [];
 
+  constructor(protected container: HTMLElement, protected options: GroupManagerOptions) {}
   public getGroups(): Group[] {
     return this.groups;
   }
@@ -22,6 +53,24 @@ export class GroupManager {
     return this.items;
   }
   public syncItems(nextItems: GroupManagerItem[]) {
-    const result = diff(this.items, nextItems, ({ key }) => key);
+    const container = this.container;
+    const prevGroupKeys = this.groupKeys;
+    const nextManagerGroups = categorize(nextItems);
+    const nextGroupKeys: Record<string | number, Group> = {};
+    const GridContructor = this.options.gridContructor;
+    const nextGroups: Group[] = nextManagerGroups.map(({ groupKey }) => {
+      return {
+        groupKey,
+        grid: prevGroupKeys[groupKey]?.grid ?? new GridContructor(container, {}),
+      };
+    });
+
+    nextGroups.forEach((group) => {
+      nextGroupKeys[group.groupKey] = group;
+    });
+
+    this.items = nextItems;
+    this.groups = nextGroups;
+    this.groupKeys = nextGroupKeys;
   }
 }
