6c6a4d599db2848d39f806276432609e3b5ba327
Daybrush <daybrush@gmail.com>
2021-07-16 12:22:21 +0900

    feat: add getStatus in InfiniteGridItem

--- src/InfiniteGridItem.ts
+++ src/InfiniteGridItem.ts
@@ -1,12 +1,25 @@
 import { GridItem, GridItemStatus } from "@egjs/grid";
+import { InfiniteGridItemInfo } from "./types";
 
 export interface InfiniteGridItemStatus extends GridItemStatus {
   groupKey: string | number;
+  key: string | number;
+  html: string;
 }
-export class InfiniteGridItem extends GridItem {
+
+export class InfiniteGridItem extends GridItem implements Required<InfiniteGridItemInfo> {
   public groupKey: string | number;
+  public html = "";
   constructor(horizontal: boolean, itemStatus?: Partial<InfiniteGridItemStatus>) {
     super(horizontal, itemStatus);
   }
+  public getStatus(): Required<InfiniteGridItemStatus> {
+    return {
+      ...super.getStatus(),
+      groupKey: this.groupKey,
+      html: this.html,
+    };
+  }
 }
+
 export interface InfiniteGridItem extends Required<InfiniteGridItemStatus> {}
