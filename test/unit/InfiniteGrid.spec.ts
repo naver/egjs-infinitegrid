00bab09ec18002d70366df12bd25eec03bbe80a5
Daybrush <daybrush@gmail.com>
2021-07-19 02:51:56 +0900

    test: test InfiniteGrid methods

---
+++ test/unit/InfiniteGrid.spec.ts
@@ -0,0 +1,184 @@
+import { cleanup, sandbox, waitEvent } from "./utils/utils";
+import InfiniteGrid, { InfiniteGridOptions } from "../../src/InfiniteGrid";
+import { SampleGrid } from "./samples/SampleGrid";
+import { toArray } from "../../src/utils";
+
+describe("test InfiniteGrid", () => {
+  let ig: InfiniteGrid | null;
+  let container: HTMLElement | null;
+  beforeEach(() => {
+    container = sandbox("")!;
+    container!.style.cssText = "";
+  });
+
+  afterEach(() => {
+    if (ig) {
+      ig!.destroy();
+    }
+    ig = null;
+    container = null;
+    cleanup();
+  });
+  describe("Initialzation", () => {
+    it("should check if container elements are the same", () => {
+      // Given
+      container!.innerHTML = `
+      <div class="wrapper" style="width: 100%; height: 500px;">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+      </div>
+      `;
+      const wrapper = container!.querySelector<HTMLElement>(".wrapper")!;
+      ig = new InfiniteGrid<InfiniteGridOptions>(wrapper, {
+        gridConstructor: SampleGrid,
+        container: true,
+      });
+
+      // When, Then
+      expect(ig!.getWrapperElement()).to.be.equals(wrapper);
+      expect(ig!.getScrollContainerElement()).to.be.equals(wrapper);
+      expect(ig!.getItems().length).to.be.equals(0);
+    });
+  });
+  describe("test rendering", () => {
+    beforeEach(() => {
+      // Given
+      container!.innerHTML = `
+      <div class="wrapper" style="width: 100%; height: 500px;">
+      </div>
+      `;
+      const wrapper = container!.querySelector<HTMLElement>(".wrapper")!;
+      ig = new InfiniteGrid<InfiniteGridOptions>(wrapper, {
+        gridConstructor: SampleGrid,
+        container: true,
+      });
+    });
+    it("should check to render children when items are not present", async () => {
+      // Given
+      const igContainer = ig!.getContainerElement();
+
+      igContainer.innerHTML = `<div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+      <div>5</div>
+      <div>6</div>`;
+      const children = toArray(igContainer.children);
+
+      // When
+      ig!.renderItems();
+
+      await waitEvent(ig!, "renderComplete");
+
+
+      const firstItem = ig!.getItems()[0];
+
+      // Then
+      ig!.getItems().forEach((item) => {
+        expect(item.groupKey).to.be.equals(firstItem.groupKey);
+      });
+      ig!.getGroups().forEach((group) => {
+        expect(group.groupKey).to.be.equals(firstItem.groupKey);
+      });
+      ig!.getVisibleGroups().forEach((group) => {
+        expect(group.groupKey).to.be.equals(firstItem.groupKey);
+      });
+      children.forEach((child, i) => {
+        expect(child.style.top).to.be.equals(`${i * 18}px`);
+      });
+    });
+    it("should check if items are rendered", async () => {
+      // Given
+      const igContainer = ig!.getContainerElement();
+
+      ig!.syncItems([0, 1, 2, 3, 4, 5].map((child) => {
+        return {
+          groupKey: Math.floor(child / 3),
+          key: child,
+          html: `<div>${child}</div>`,
+        };
+      }));
+      // When
+      ig!.setCursors(0, 1);
+
+      await waitEvent(ig!, "renderComplete");
+      const children = toArray(igContainer.children);
+
+      // Then
+      expect(ig!.getItems().map((item) => item.groupKey)).to.be.deep.equals([0, 0, 0, 1, 1, 1]);
+      expect(ig!.getGroups().map((group) => group.groupKey)).to.be.deep.equals([0, 1]);
+      expect(ig!.getVisibleGroups().map((group) => group.groupKey)).to.be.deep.equals([0, 1]);
+      expect(children.length).to.be.equals(6);
+
+      children.forEach((child, i) => {
+        expect(child.style.top).to.be.equals(`${i * 18}px`);
+      });
+    });
+    it("should check if only visible items are rendered", async () => {
+      // Given
+      const igContainer = ig!.getContainerElement();
+
+      ig!.syncItems([0, 1, 2, 3, 4, 5].map((child) => {
+        return {
+          groupKey: Math.floor(child / 3),
+          key: child,
+          html: `<div>${child}</div>`,
+        };
+      }));
+
+      ig!.setCursors(0, 1);
+
+      await waitEvent(ig!, "renderComplete");
+
+      // When
+      // [0, 1] => [1]
+      ig!.setCursors(1, 1);
+
+      await waitEvent(ig!, "renderComplete");
+      const children = toArray(igContainer.children);
+
+      // Then
+      expect(ig!.getVisibleGroups().map((group) => group.groupKey)).to.be.deep.equals([1]);
+      expect(children.length).to.be.equals(3);
+
+      children.forEach((child, i) => {
+        expect(child.style.top).to.be.equals(`${54 + i * 18}px`);
+      });
+    });
+    it("should check whether it is rendered after changing options", async () => {
+      // Given
+      const igContainer = ig!.getContainerElement();
+
+      ig!.syncItems([0, 1, 2, 3, 4, 5].map((child) => {
+        return {
+          groupKey: Math.floor(child / 3),
+          key: child,
+          html: `<div>${child}</div>`,
+        };
+      }));
+
+      ig!.setCursors(0, 1);
+
+      await waitEvent(ig!, "renderComplete");
+
+
+      // When
+      // 0 => 5
+      ig!.gap = 5;
+      ig!.renderItems();
+
+      await waitEvent(ig!, "renderComplete");
+
+      // Then
+      const children = toArray(igContainer.children);
+
+      children.forEach((child, i) => {
+        expect(child.style.top).to.be.equals(`${i * 23}px`);
+      });
+    });
+  });
+});
