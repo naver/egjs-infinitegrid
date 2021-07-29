b8767f0f3901c2dd203af5fab54b88193a0c7493
Daybrush <daybrush@gmail.com>
2021-07-29 15:59:44 +0900

    feat: add exports

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import InfiniteGrid from "./InfiniteGrid";
+export * from "./InfiniteGrid";
+export * from "./grids/MasonryInfiniteGrid";
+export * from "./grids/JustifiedInfiniteGrid";
+export * from "./grids/FrameInfiniteGrid";
+export * from "./grids/PackingInfiniteGrid";
+export * from "./types";
+export * from "./consts";
+export default InfiniteGrid;
