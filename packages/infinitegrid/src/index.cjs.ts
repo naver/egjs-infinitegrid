8162457b0beae92966096cb2202a3aedcf185809
Daybrush <younkue.choi@navercorp.com>
2022-08-18 13:24:17 +0900

    fix: declare module type

--- packages/infinitegrid/src/index.cjs.ts
+++ packages/infinitegrid/src/index.cjs.ts
@@ -4,6 +4,7 @@ for (const name in modules) {
   (InfiniteGrid as any)[name] = (modules as any)[name];
 }
 
+declare const module: any;
 module.exports = InfiniteGrid;
 export default InfiniteGrid;
 export * from "./index";
